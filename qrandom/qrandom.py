20f08444ef0467225cbc2597fc08b0319ab938dc
hannahyelle <hannah.yelle.4@gmail.com>
2023-01-28 10:49:54 -0500

    package function defs

---
+++ qrandom/qrandom.py
@@ -0,0 +1,12 @@
+"""
+This module utilizes quantum computers 
+to produce truly random numbers
+"""
+
+#this function returns a random integer
+def randint():
+    pass
+
+#this function returns a random integer between x (inclusive) and y (exclusive)
+def randrange(x,y):
+    pass
