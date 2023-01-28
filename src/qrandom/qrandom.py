aec49dcece2f2cf61aef687f1432b2d0d0b3e110
hannahyelle <hannah.yelle.4@gmail.com>
2023-01-28 13:38:45 -0500

    added pre-commit

--- src/qrandom/qrandom.py
+++ src/qrandom/qrandom.py
@@ -3,10 +3,12 @@ This module utilizes quantum computers
 to produce truly random numbers
 """
 
-#this function returns a random integer
+
+# this function returns a random integer
 def randint():
     return 2
 
-#this function returns a random integer between x (inclusive) and y (exclusive)
-def randrange(x,y):
+
+# this function returns a random integer between x (inclusive) and y (exclusive)
+def randrange(x, y):
     pass
--- tests/test_qrandom.py
+++ tests/test_qrandom.py
@@ -1,7 +1,7 @@
 import pytest
-import numpy as np 
 from qoalas.src.qrandom.qrandom import randint
 
-@pytest.fixture 
+
+@pytest.fixture
 def test_randint():
     assert type(randint()), int
